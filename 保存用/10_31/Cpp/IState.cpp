2eb6bf783c0e28fe6175387cadf8dd04da1da4fe
TICNET\student3 <kagato180717@gmail.com>
2025-10-31 11:36:23 +0900

    不要ファイル削除

--- 保存用/10_31/Cpp/IState.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Header/IState.h"
-
-#include "../Header/KeyState.h"
-#include "../Header/Master.h"
-
-IState_SetPlayerDataUI::IState_SetPlayerDataUI()
-{
-	mpKeyState = Master::mpKeyState;
-}
